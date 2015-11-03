cc283d044723cc37b0f7170a07387c34b4a48947
Adam Balogh <balogh.adam@icloud.com>
2015-11-03 13:38:03 +0000

    move random number generator to util

--- util.h
+++ util.h
@@ -1,5 +1,9 @@
+#pragma once
+
 #include <string>
+
 #include "audit/third_party/cryptopp/integer.h"
+#include "audit/third_party/cryptopp/osrng.h"
 
 namespace audit {
 
@@ -13,4 +17,23 @@ inline void StringToCryptoInteger(const std::string& in,
                                   CryptoPP::Integer* out) {
   out->Decode((unsigned char*)in.data(), in.size());
 }
+
+// A generic interface for random number generation
+class RandomNumberGenerator {
+ public:
+  // Returns a random number x, where x >= 0 and x < max
+  virtual CryptoPP::Integer GenerateNumber(const CryptoPP::Integer& max) = 0;
+  virtual ~RandomNumberGenerator() {}
+};
+
+// RandomNumberGenerator implementation using CryptoPP
+class CryptoNumberGenerator : public RandomNumberGenerator {
+ public:
+  CryptoPP::Integer GenerateNumber(const CryptoPP::Integer& max) override {
+    return CryptoPP::Integer{g, 0, max};
+  }
+
+ private:
+  CryptoPP::AutoSeededRandomPool g;
+};
 }
