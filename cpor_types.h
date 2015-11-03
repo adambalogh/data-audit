010773c34891f3897ac39b77e3112504031a15e2
Adam Balogh <balogh.adam@icloud.com>
2015-11-03 13:37:44 +0000

    fiddlng with types

--- cpor_types.h
+++ cpor_types.h
@@ -12,6 +12,14 @@ namespace audit {
 
 typedef unsigned char byte;
 
+struct FileTag {
+  unsigned long num_blocks;
+  unsigned long num_sectors;
+  int sector_size;
+  std::vector<CryptoPP::Integer> alphas;
+  CryptoPP::Integer p;
+};
+
 struct SecretKeys {
   std::array<byte, CryptoPP::HMAC<CryptoPP::SHA512>::DEFAULT_KEYLENGTH> mac_key;
   std::array<byte, CryptoPP::AES::DEFAULT_KEYLENGTH> enc_key;
