e860fac9c1365152460cf1e1ffc6c1ca9cd4a4ea
Adam Balogh <balogh.adam@icloud.com>
2015-11-09 12:06:08 +0000

    common.h updates

--- common.h
+++ common.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <memory>
+
 #include "openssl/bn.h"
 
 namespace audit {
 
 typedef unsigned char byte;
-
-using BN_ptr = std::unique_ptr<BIGNUM, decltype(&::BN_free)>;
+typedef std::unique_ptr<BIGNUM, decltype(&::BN_free)> BN_ptr;
+typedef std::unique_ptr<BN_CTX, decltype(&::BN_CTX_free)> BN_CTX_ptr;
 }
