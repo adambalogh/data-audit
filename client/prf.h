8c6ac32c78b8ec0b68cf09a501fd18b075e2a36b
Adam Balogh <balogh.adam@icloud.com>
2015-11-17 17:11:34 +0000

    prf comments

--- client/prf.h
+++ client/prf.h
@@ -12,17 +12,26 @@
 
 namespace audit {
 
+// PRF is a cryptographic algorithm that encodes an unsigned integer using a
+// secret key.
 class PRF {
  public:
+  PRF(const unsigned char* key, int length) : key_(key), length_(length) {}
   virtual BN_ptr Encode(unsigned int i) = 0;
   virtual ~PRF() {}
+
+ protected:
+  const unsigned char* key_;
+  int length_;
 };
 
+// HMAC-SHA1 based PRF implementation
 class HMACPRF : public PRF {
  public:
-  HMACPRF(const unsigned char* key, int length) : hmac_(key, length) {}
+  HMACPRF(const unsigned char* key, int length)
+      : PRF(key, length), hmac_(key_, length_) {}
   HMACPRF(const std::string& key)
-      : hmac_((unsigned char*)key.data(), key.size()) {}
+      : HMACPRF((unsigned char*)key.data(), key.size()) {}
 
   BN_ptr Encode(unsigned int i);
 
