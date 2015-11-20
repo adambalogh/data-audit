f5f79796a6157a19fbebbec36e72c6d770438e0c
Adam Balogh <balogh.adam@icloud.com>
2015-11-20 15:53:55 +0000

    num blocks test

--- file_tag_test.cc
+++ file_tag_test.cc
@@ -37,7 +37,9 @@ TEST(FileTag, NumBlocks) {
   std::stringstream file{"aaaaaaaaa"};
   ConstantNumberGenerator<1> g;
   BN_ptr p{BN_new(), ::BN_free};
-  FileTag tag{file, 2, 2, std::move(p), &g};
+  FileTag tag{file, 2, 1, std::move(p), &g};
+
+  EXPECT_EQ(5, tag.num_blocks());
 }
 
 int main(int argc, char **argv) {
