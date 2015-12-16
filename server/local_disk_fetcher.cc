20982c743b391b8fb71c28850c3d32dababbe664
Adam Balogh <balogh.adam@icloud.com>
2015-12-16 01:27:54 +0000

    local disk fetcher works with local disk storage

--- server/local_disk_fetcher.cc
+++ server/local_disk_fetcher.cc
@@ -13,7 +13,7 @@ std::basic_istream<char, std::char_traits<char>>& LocalDiskFetcher::FetchBlock(
 proto::BlockTag LocalDiskFetcher::FetchBlockTag(unsigned long index) {
   proto::BlockTag tag;
   std::ifstream tag_file_;
-  tag_file_.open(path_ + "tags" + file_tag_.file_name() +
+  tag_file_.open(file_dir_ + "tags" + file_tag_.file_name() +
                  std::to_string(index));
   std::stringstream buffer;
   buffer << tag_file_.rdbuf();
--- server/local_disk_fetcher.h
+++ server/local_disk_fetcher.h
@@ -8,10 +8,8 @@ namespace audit {
 // Fetches from local disk
 class LocalDiskFetcher : public Fetcher {
  public:
-  LocalDiskFetcher(const proto::PublicFileTag& file_tag,
-                   const std::string& path)
-      : file_tag_(file_tag), path_(path) {
-    file_.open(path_ + file_tag_.file_name(), std::ifstream::binary);
+  LocalDiskFetcher(const proto::PublicFileTag& file_tag) : file_tag_(file_tag) {
+    file_.open(file_dir_ + file_tag_.file_name(), std::ifstream::binary);
   }
 
   std::basic_istream<char, std::char_traits<char>>& FetchBlock(
@@ -21,7 +19,8 @@ class LocalDiskFetcher : public Fetcher {
 
  private:
   proto::PublicFileTag file_tag_;
-  std::string path_;
+
+  std::string file_dir_{"/users/adambalogh/Developer/audit/files_dir/"};
 
   std::ifstream file_;
 };
