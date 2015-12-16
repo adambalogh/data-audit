d170dd8b07dd7287fee830e2b263de82ff4e5407
Adam Balogh <balogh.adam@icloud.com>
2015-12-16 00:27:35 +0000

    storage.h

---
+++ client/storage.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+
+#include "audit/proto/cpor.pb.h"
+
+namespace audit {
+
+class Storage {
+ public:
+  virtual void StoreBlockTag(const proto::BlockTag& tag) = 0;
+  virtual void StoreFileTag(const proto::PrivateFileTag& file_tag) = 0;
+
+  virtual ~Storage() {}
+};
+
+class StorageUsingFileName : public Storage {
+ public:
+  StorageUsingFileName(const std::string& file_name) : file_name_(file_name) {}
+
+ protected:
+  const std::string file_name_;
+};
+}
