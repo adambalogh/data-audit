28363d8384f00f6510c4ed8d3db73496c9bbe36f
Adam Balogh <balogh.adam@icloud.com>
2015-12-16 01:28:22 +0000

    main uses tagger

--- test/main.cc
+++ test/main.cc
@@ -1,8 +1,21 @@
-#include "test_client.h"
+#include <sstream>
+
+#include "audit/util.h"
+#include "audit/client/tagger.h"
+#include "audit/client/local_disk_storage.h"
+#include "audit/client/prf.h"
+
+using namespace audit;
 
 int main() {
-  TestClient main{
+  Tagger tagger{
+      std::unique_ptr<Storage>{new LocalDiskStorage{"test"}},
+      std::unique_ptr<RandomNumberGenerator>{new CryptoNumberGenerator},
+      std::unique_ptr<PRF>{new HMACPRF{"code"}}};
+
+  std::stringstream file{
       "aejfwoigjqogijwer;goit43io;h5w3[94thg39wa;wighe;oiw4h3;"
       "toihtq09rhgwqpg84538h"};
-  main.Run();
+
+  tagger.Run(file, "test");
 }
