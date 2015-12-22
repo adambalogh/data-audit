3e1f6cc4de847718c1d5177ce26697a9d6b11fb9
Adam Balogh <balogh.adam@icloud.com>
2015-12-22 15:25:30 +0100

    app basic html

--- client/amazon_s3_storage.h
+++ client/amazon_s3_storage.h
@@ -2,6 +2,8 @@
 
 #include <istream>
 
+#include <aws/s3/S3Client>
+
 #include "audit/client/storage.h"
 #include "audit/proto/cpor.pb.h"
 
@@ -9,8 +11,14 @@ namespace audit {
 
 class AmazonS3Storage : public Storage {
  public:
+  AmazonS3Storage(const Aws::Auth::AWSCredentials& credentials)
+      : client_(credentials) {}
+
   virtual void StoreBlockTag(const proto::BlockTag& tag);
   virtual void StoreFileTag(const proto::PrivateFileTag& file_tag);
   virtual void StoreFile(std::istream& file);
+
+ private:
+  Aws::S3::S3Client client_;
 };
 }
