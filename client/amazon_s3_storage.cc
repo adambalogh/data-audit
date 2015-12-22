851be8257ce523b66bf50c58ac53bdace3cc5f40
Adam Balogh <balogh.adam@icloud.com>
2015-12-22 12:26:42 +0100

    amazon s3 storage skeleton

--- client/amazon_s3_storage.cc
+++ client/amazon_s3_storage.cc
@@ -1,1 +1,10 @@
 #include "audit/client/amazon_s3_storage.h"
+
+namespace audit {
+
+void AmazonS3Storage::StoreBlockTag(const proto::BlockTag& tag) {}
+
+void AmazonS3Storage::StoreFileTag(const proto::PrivateFileTag& file_tag) {}
+
+void AmazonS3Storage::StoreFile(std::istream& file) {}
+}
--- client/amazon_s3_storage.h
+++ client/amazon_s3_storage.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <istream>
+
+#include "audit/client/storage.h"
+#include "audit/proto/cpor.pb.h"
+
 namespace audit {
 
-class AmazonS3Storage {};
+class AmazonS3Storage : public Storage {
+ public:
+  virtual void StoreBlockTag(const proto::BlockTag& tag);
+  virtual void StoreFileTag(const proto::PrivateFileTag& file_tag);
+  virtual void StoreFile(std::istream& file);
+};
 }
