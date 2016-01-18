1e1b6ac622c5d0e7e810b41d04d463e1b0ffb4be
Adam Balogh <balogh.adam@icloud.com>
2016-01-18 23:01:13 +0000

    click on file to verify

--- app/index.js
+++ app/index.js
@@ -7,29 +7,35 @@ window.onload = function() {
   var file_browser = require('bindings')('file_browser');
 
   var files_list = document.getElementById("files");
+
   file_browser.show_files(function(files) {
+
     files.forEach(function(file) {
       var li = document.createElement("li");
       li.appendChild(document.createTextNode(file));
+      li.setAttribute("class", "file");
       files_list.appendChild(li);
     });
+
+    var file_buttons = document.getElementsByClassName("file");
+    for (var i = 0; i < file_buttons.length; i++) {
+      file_buttons[i].onclick = function(e) {
+        var fileName = this.innerText;
+        verify.verify(fileName, function(result) {
+          alert(result);
+        });
+      }
+    }
+
   });
 
   var uploadButton = document.getElementById("upload");
+
   uploadButton.onclick = function() {
     dialog.showOpenDialog(function(files) {
       upload.upload(files[0], function(a) {
         alert(a);
       });
-    });
-  }
-
-  var verifyButton = document.getElementById("verify");
-  
-  verifyButton.onclick = function() {
-    var fileName = document.getElementById("input").value;
-    verify.verify(fileName, function(result) {
-      alert(result);
     });
   }
 
