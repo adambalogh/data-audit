d91c7e9bba65830a7c52c04d6d95a4625bf34030
Adam Balogh <balogh.adam@icloud.com>
2016-02-05 22:07:46 +0000

    authorizeurl test

--- providers/dropbox/token_source_test.cc
+++ providers/dropbox/token_source_test.cc
@@ -5,6 +5,8 @@
 
 #include "audit/common.h"
 
+#include "cpprest/uri.h"
+
 using namespace audit::providers::dropbox;
 
 void RemoveTokenFile() { std::remove(TokenSource::TOKEN_FILE.c_str()); }
@@ -55,6 +57,25 @@ TEST_F(TokenSourceTest, RetrieveTokenFromFile) {
   EXPECT_FALSE(source.NeedToAuthorize());
   EXPECT_EQ(token, source.GetToken());
 }
+
+TEST_F(TokenSourceTest, AuthorizeUrl) {
+  TokenSource source;
+  auto url_str = source.GetAuthorizeUrl();
+  web::uri url{url_str};
+  auto query = web::uri::split_query(url.query());
+
+  EXPECT_EQ("https", url.scheme());
+  EXPECT_EQ("www.dropbox.com", url.host());
+  EXPECT_EQ("/1/oauth2/authorize", url.path());
+
+  EXPECT_EQ("code", query["response_type"]);
+  EXPECT_EQ("1234", query["client_id"]);
+}
+
+// TEST_F(TokenSourceTest, ExchangeCodeForToken) {
+//  RemoveTokenFile();
+//  TokenSource source;
+//}
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
