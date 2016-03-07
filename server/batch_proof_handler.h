44d295fd95a669777d4cbe98da108420cb1a2b29
adambalogh <baloghadam@dataaudit.h4ianu02ljuuzgdqtfp1zly1rf.ax.internal.cloudapp.net>
2016-03-07 18:06:58 +0000

    batch proof handler

--- server/batch_proof_handler.h
+++ server/batch_proof_handler.h
@@ -1,33 +1,36 @@
 #pragma once
 
+#include <memory>
+
 #include "proxygen/httpserver/RequestHandler.h"
 
 #include "audit/server/fetcher.h"
+#include "audit/providers/local_disk/fetcher.h"
 #include "audit/client/verify/no_server_proof_source.h"
 
 namespace audit {
 namespace server {
 
 class BatchProofHandler : public proxygen::RequestHandler {
- public:
-  void onRequest(
-      std::unique_ptr<proxygen::HTTPMessage> headers) noexcept override;
+   public:
+    void onRequest(
+        std::unique_ptr<proxygen::HTTPMessage> headers) noexcept override;
 
-  void onBody(std::unique_ptr<folly::IOBuf> body) noexcept override;
+    void onBody(std::unique_ptr<folly::IOBuf> body) noexcept override;
 
-  void onEOM() noexcept override;
+    void onEOM() noexcept override;
 
-  void onUpgrade(proxygen::UpgradeProtocol proto) noexcept override;
+    void onUpgrade(proxygen::UpgradeProtocol proto) noexcept override;
 
-  void requestComplete() noexcept override;
+    void requestComplete() noexcept override;
 
-  void onError(proxygen::ProxygenError err) noexcept override;
+    void onError(proxygen::ProxygenError err) noexcept override;
 
- private:
-  verify::NoServerProofSource proof_source_{
-      std::make_unique<local_disk::FetcherFactory>()};
+   private:
+    audit::verify::NoServerProofSource proof_source_{
+        std::unique_ptr<audit::server::FetcherFactory>()};
 
-  std::unique_ptr<folly::IOBuf> body_;
+    std::unique_ptr<folly::IOBuf> body_;
 };
 }
 }
